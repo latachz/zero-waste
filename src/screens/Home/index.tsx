f8689dd4133073c4829bb768257c4ca7d36ca38a
Adam Kopczynski <akadamkopczynski@gmail.com>
2020-11-27 19:53:14 +0100

    Fixed svgs

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import {View, Text, StatusBar} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
-import Logo from '../../../assets/images/recycle.svg';
+//@ts-ignore
+import Logo from '../../images/recycle.svg';
 
 import styles from './styles';
 import colors from '../../theme/colors';
 
-const Login: React.FC = () => {
+const Home: React.FC = () => {
   return (
     <View style={styles.screen}>
       <StatusBar backgroundColor="#fff" barStyle="dark-content" />
-      <Logo />
+      <Logo width={40} height={40} />
       <Text style={styles.title}>Save the planet</Text>
       <Text style={styles.description}>Save the planet</Text>
     </View>
   );
 };
 
-export default Login;
+export default Home;
